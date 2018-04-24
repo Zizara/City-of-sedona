cf9d69f9b4bb21b257a5c1b24f6dfade634d152b
Elizarov Andrei <elizarov13@yandex.ru>
2018-04-24 23:19:23 +0300

    input range Rev 01

---
+++ js/slider.js
@@ -0,0 +1,15 @@
+$("#slider").slider({
+	min: 0,
+	max: 3000,
+	values: [0,3000],
+	range: true,
+  step: 5,
+	// stop: function(event, ui) {
+	// 	$("input#minCost").val($("#slider").slider("values",0));
+	// 	$("input#maxCost").val($("#slider").slider("values",1));
+  //   },
+    slide: function(event, ui){
+  		$("input#minCost").val("от "+ $("#slider").slider("values",0));
+  		$("input#maxCost").val("до "+ $("#slider").slider("values",1));
+    }
+});
