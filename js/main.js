2280be39cd1a01c841c0b57120cb52f9cea89d36
Elizarov Andrei <elizarov13@yandex.ru>
2018-05-24 22:40:42 +0300

    add jsSort

--- js/main.js
+++ js/main.js
@@ -68,3 +68,67 @@ $("#slider").slider({
 				countInputChild.value = parseInt(countInputChild.value)-1+unitsChild;
 		}
 	};
+
+// -----------------Сортировка-----------------//
+
+
+var buttonDown = document.getElementsByTagName("button")[0];
+var buttonUp = document.getElementsByTagName("button")[1];
+var buttonType = document.getElementsByTagName("button")[2];
+
+buttonDown.onclick = function () {
+	//получаем элемент с id center
+	var div = document.getElementById ('center');
+	//получаем в id center все элементы с классом content
+	var content = div.querySelectorAll ('.content');
+	//получаем в id center все элементы с классом price
+	var prices = div.querySelectorAll ('.price');
+
+	//создаем новый ассоциативный массив list с длиной равное коллекции price
+	for (var list = [], j = 0, le = prices.length; j < le; j++) {
+		list[j] ={}
+	//создаем ключ ассоциативного массива с именем prc, записываем внутрене содержание spana в prc
+		list[j].prc= +prices [j].innerHTML;
+	//создаем ключ ассоциативного массива с именем cln, делаем клон diva с дочерними элементами (true)
+		list[j].cln = content [j].cloneNode (true);
+	};
+	//опустошаем внутренние содеражание diva c id='center'
+	div.innerHTML ='';
+	//сортируем под убыванию divы
+	list.sort (function (x, y) {return y.prc - x.prc});
+	//Добавляет (appendChild) элемент (list [j].cln) в конец переменной div
+    for (j = 0; j < le; j++) div.appendChild (list [j].cln);
+}
+
+
+ buttonUp.onclick = function () {
+	var div = document.getElementById ('center');
+	var content = div.querySelectorAll ('.content');
+	var prices = div.querySelectorAll ('.price');
+
+
+	for (var list = [], j = 0, le = prices.length; j < le; j++) {
+		list[j] ={}
+		list[j].prc= +prices [j].innerHTML;
+		list[j].cln = content [j].cloneNode (true);
+	};
+	div.innerHTML ='';
+	list.sort (function (x, y) {return x.prc - y.prc});
+    for (j = 0; j < le; j++) div.appendChild (list [j].cln);
+}
+
+ buttonType.onclick = function () {
+	var div = document.getElementById ('center');
+	var content = div.querySelectorAll ('.content');
+	var type = div.querySelectorAll ('.type');
+
+
+	for (var list = [], j = 0, le = type.length; j < le; j++) {
+		list[j] ={}
+		list[j].tp= +type [j].innerHTML;
+		list[j].cln = content [j].cloneNode (true);
+	};
+	div.innerHTML ='';
+	list.sort (function (x, y) {return x.tp - y.tp});
+    for (j = 0; j < le; j++) div.appendChild (list [j].cln);
+}
